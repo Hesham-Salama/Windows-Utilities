ba4c4e2c3fccb59a9c30d3bf81d3ca9345a8b570
Hesham Salama <theflash1970@gmail.com>
2018-07-30 19:07:49 +0200

    Update SystemInfo.py

--- SystemInfo.py
+++ SystemInfo.py
@@ -9,18 +9,6 @@ import io
 # Downloaded Libraries
 # 1- psutil , to install: CMD: C:\Python27\python.exe -m pip install psutil
 import psutil
-import ctypes
-
-class disable_file_system_redirection:
-    _disable = ctypes.windll.kernel32.Wow64DisableWow64FsRedirection
-    _revert = ctypes.windll.kernel32.Wow64RevertWow64FsRedirection
-    def __enter__(self):
-        self.old_value = ctypes.c_long()
-        self.success = self._disable(ctypes.byref(self.old_value))
-    def __exit__(self, type, value, traceback):
-        if self.success:
-            self._revert(self.old_value)
-
 
 class SystemInfo:
     dictOfPids = {}
